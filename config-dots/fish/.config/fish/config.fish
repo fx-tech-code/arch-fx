c9f66136574c286db31b20bd0fc90a8da0143849
fx <felix.mail@posteo.de>
2026-04-17 23:48:08 +0200

    initial commit

---
+++ config-dots/fish/.config/fish/config.fish
@@ -0,0 +1,44 @@
+#source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+#   echo "Welcome to Arch Linux! Type 'fastfetch' to see your system info."
+end
+
+function fish_prompt
+    set -l last_status $status
+
+    set -l c_reset (set_color normal)
+    set -l c_path (set_color blue)
+    set -l c_git (set_color brblack)
+    set -l c_ok (set_color magenta)
+    set -l c_err (set_color red)
+
+    # First line: current directory + git info
+    echo -n $c_path(prompt_pwd)
+
+    if command -sq git; and command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        set -l branch (command git symbolic-ref --quiet --short HEAD 2>/dev/null; or command git rev-parse --short HEAD 2>/dev/null)
+        if test -n "$branch"
+            echo -n " " $c_git$branch
+
+            command git diff --no-ext-diff --quiet --exit-code 2>/dev/null
+            or echo -n "*"
+
+            command git diff --no-ext-diff --cached --quiet --exit-code 2>/dev/null
+            or echo -n "*"
+        end
+    end
+
+    echo
+
+    # Second line: prompt symbol, colored by last exit status
+    if test $last_status -eq 0
+        echo -n $c_ok"❯ "
+    else
+        echo -n $c_err"❯ "
+    end
+
+    echo -n $c_reset
+end
